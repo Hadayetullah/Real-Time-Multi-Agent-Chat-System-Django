e2acce42a33a18b6c4e2453a7185035c587a631a
Hadayetullah <hadayetullah30@gmail.com>
2026-01-27 21:39:28 +0600

    Initialize frontend

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function VisitorPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1>Visitor Page</h1>
+    </div>
+  );
+}
